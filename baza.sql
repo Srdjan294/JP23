4b459a69616d319b8b2b3c1b55fe3f1c7b8d66d0
Srdjan294 <srdjanfilipovic991@gmail.com>
2020-12-09 17:25:53 +0100

    promjene na bazi

--- baza.sql
+++ baza.sql
@@ -10,50 +10,27 @@ create table auto(
 	registracija varchar(20) not null
 );
 
-#Izračun potrošnje auta prema natočenim litrama goriva i prema natočenom gorivu za određene novce
-create table potrosnja(
-	sifra	int not null primary key auto_increment,
-	datum	datetime not null,
-	natocenoLitara int not null,
-	cijenaL  int not null,
-	ukupnaCijena int not null,
-	pocetnoStanje int not null,
-	zavrsnoStanje int not null,
-	udaljenost int not null, 
-	potrosnjaLPoPrijedenimKm int not null, #natocenoLitara/udaljenost * 100(km)
-	potrosnjaKnPoPrijedenimKm int not null #ukupnaCijena/udaljenost * 100(km)
-);
-
-#Izračun prelaska kilometara prema natočenim litrama goriva i prema natočenom gorivu za određene novce
-create table izracunPrelaskaKm(
-	sifra int not null primary key auto_increment,
-	natocenoLitara int not null,
-	cijenaL  int not null,
-	ukupnaCijena int not null,
-	potrosnjaAuta int not null,
-	izracunKM_L int not null, #100/potrosnjaAuta * natocenoLitara
-	izracunKM_Kn int not null #ukupnaCijena / cijenaL = natocenoLitara * 100/potrosnjaAuta
-);
-
-#Izračun natočenih litara i cijene goriva prema prijeđenim kilometrima
-create table izracunGoriva(
-	sifra int not null primary key auto_increment,
-	udaljenost int not null,
-	cijenaL int not null,
-	potrosnjaAuta int not null,
-	izracunL int not null, #potrosnjaAuta/100 * udaljenost
-	ukupnaCijena int not null #potrosnjaAuta/100 * udaljenost * cijenaL
-);
-
 create table evidencija(
 	sifra int not null primary key auto_increment,
 	auto int not null,
-	potrosnja int not null,
-	izracunPrelaskaKm int not null,
-	izracunGoriva int not null
+	datum	datetime not null,
+	natocenoLitara int not null,
+	ukupnaCijena int not null,
+	pocetnoStanje int not null,
+	zavrsnoStanje int not null,
+	udaljenost int not null	
+);
+
+create table oznaka(
+	sifra int not null primary key auto_increment,
+	naziv varchar(150)
+);
+
+create table evidencija_oznaka(
+	evidencija int not null,
+	oznaka int not null
 );
 
 alter table evidencija add foreign key (auto) references auto(sifra);
-alter table evidencija add foreign key (potrosnja) references potrosnja(sifra);
-alter table evidencija add foreign key (izracunPrelaskaKm) references izracunPrelaskaKm(sifra);
-alter table evidencija add foreign key (izracunGoriva) references izracunGoriva(sifra);
+alter table evidencija_oznaka add foreign key (evidencija) references evidencija(sifra);
+alter table evidencija_oznaka add foreign key (oznaka) references oznaka(sifra);
