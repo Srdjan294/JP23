6a839c5b41ee342ff7c56b620d9c808369eaac8d
Srdjan294 <srdjanfilipovic991@gmail.com>
2020-12-25 15:54:15 +0100

    riješen 8. zadatak

--- eclipse/Edunova02/src/edunova/zadaci/Zadatak8.java
+++ eclipse/Edunova02/src/edunova/zadaci/Zadatak8.java
@@ -1,11 +1,24 @@
 package edunova.zadaci;
 
+import javax.swing.JOptionPane;
+
 public class Zadatak8 {
 
-	//. Napiši program koji odreðuje površinu trokuta zadanih stranica.
+	// Napiši program koji odreðuje površinu trokuta zadanih stranica.
 	
 	public static void main(String[] args) {
 		
+		String stranicaa = JOptionPane.showInputDialog("Unesi duljinu stranice a");
+		String stranicab = JOptionPane.showInputDialog("Unesi duljinu stranice b");
+		String stranicac = JOptionPane.showInputDialog("Unesi duljinu stranice c");
+		
+		double a = Double.parseDouble(stranicaa);
+		double b = Double.parseDouble(stranicab);
+		double c = Double.parseDouble(stranicac);
+		
+		double s = (a + b + c) / 2;
+		
+		System.out.println( Math.sqrt(s * (s - a) * (s - b) * (s - c)));
 	}
 
 }
