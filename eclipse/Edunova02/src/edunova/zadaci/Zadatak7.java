c244ae4e02700f9a1dd62c074fd3ecdbd94a756d
Srdjan294 <srdjanfilipovic991@gmail.com>
2020-12-25 15:04:46 +0100

    riješen 7. zadatak

--- eclipse/Edunova02/src/edunova/zadaci/Zadatak7.java
+++ eclipse/Edunova02/src/edunova/zadaci/Zadatak7.java
@@ -1,5 +1,7 @@
 package edunova.zadaci;
 
+import javax.swing.JOptionPane;
+
 public class Zadatak7 {
 
 	// Program od korisnika unosi slovo
@@ -8,6 +10,27 @@ public class Zadatak7 {
 
 	public static void main(String[] args) {
 		
+		char  slovo = JOptionPane.showInputDialog("Unesi slovo do A do E").charAt(0);
+				
+		switch(Character.toUpperCase(slovo)) {
+		case 'A':
+			System.out.println("1. mjesto");
+			break;
+		case 'B':
+			System.out.println("2. mjesto");
+			break;
+		case 'C':
+			System.out.println("3. mjesto");
+			break;
+		case 'D':
+			System.out.println("4. mjesto");
+			break;
+		case 'E':
+			System.out.println("5. mjesto");
+			break;
+		default:
+			System.out.println("ostalo");
+		}
 		
 	}
 }
