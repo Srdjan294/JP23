75621f6e1d280996d8f4818f32318ff048abf3b9
Srdjan294 <srdjanfilipovic991@gmail.com>
2020-12-25 14:43:29 +0100

    riješen 6. zadatak

--- eclipse/Edunova02/src/edunova/zadaci/Zadatak6.java
+++ eclipse/Edunova02/src/edunova/zadaci/Zadatak6.java
@@ -1,5 +1,7 @@
 package edunova.zadaci;
 
+import javax.swing.JOptionPane;
+
 public class Zadatak6 {
 	
 	// Korisnik unosi 2 broja. 
@@ -8,6 +10,19 @@ public class Zadatak6 {
 	// inaèe ispisuje cijeli dio kvocjenta prvog i drugog broja
 	
 	public static void main(String[] args) {
+		
+		String prviBroj= JOptionPane.showInputDialog("Unesi 1. broj");
+		String drugiBroj= JOptionPane.showInputDialog("Unesi 2. broj");
+		
+		int a = Integer.parseInt(prviBroj);
+		int b = Integer.parseInt(drugiBroj);
+		
+		
+		if((a*b)%2==0) {
+			System.out.println(a>b ? a-b : b-a );
+		}else {
+			System.out.println(a>b ? (int)a/b : (int)b/a);
+		}
 		
 	}
 
