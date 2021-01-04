67d6e38875df02ffb0893fde61e153cc6dc4b706
Srdjan294 <srdjanfilipovic991@gmail.com>
2021-01-04 14:56:35 +0100

    riješen 4. zadatak

---
+++ eclipse/ZimskiZadatak/src/zadaci/Zadatak4.java
@@ -0,0 +1,69 @@
+package zadaci;
+
+import java.util.Scanner;
+
+public class Zadatak4 {
+	
+	//Kreirati program koji unosi dvije matrice 4x4 te ispisuje njihov zbroj
+	
+	public static void main(String[] args) {
+		
+		int p = 4, q = 4, m = 4, n = 4;
+		Scanner s = new Scanner(System.in);
+		
+		
+		
+		if(p == m && q == n) {
+			int a[][] = new int[p][q];
+			int b[][] = new int[m][n];
+			int c[][] = new int[m][n];
+			
+			System.out.println("Unesi sve elemente 1. matrice: ");
+			for(int i = 0; i < p; i++) {
+				for(int j = 0; j < q; j++) {
+					a[i][j] = s.nextInt();				
+				}
+			}
+			System.out.println("Unesi sve elemente 2. matrice: ");
+			for(int i = 0; i < m; i++) {
+				for(int j = 0; j < n; j++) {
+					b[i][j] = s.nextInt();
+				}
+			}
+			System.out.println("1. matrica: ");
+			for(int i = 0; i < p; i++) {
+				for(int j = 0; j < q; j++) {
+					System.out.printf("%3d",a[i][j]);
+				}
+				System.out.println("");
+			}
+			System.out.println("2. matrica: ");
+			for(int i = 0; i < m; i++) {
+				for(int j = 0; j < n; j++) {
+					System.out.printf("%3d",b[i][j]);
+				}
+				System.out.println("");
+			}
+			
+			for(int i = 0; i < p; i++) {
+				for(int j = 0; j < n; j++) {
+					for(int k = 0; k < q; k++) {
+						c[i][j] = a[i][j] + b[i][j];
+					}
+				}
+			}
+			System.out.println("Zbroj matrica: ");
+			for(int i = 0; i < p; i++) {
+				for(int j = 0; j < n; j++) {
+					System.out.printf("%3d",c[i][j]);
+				}
+				System.out.println("");
+			}
+			
+		}
+		else {
+			System.out.println("Zbrajanje nije moguæe");
+		}
+	}
+
+}
