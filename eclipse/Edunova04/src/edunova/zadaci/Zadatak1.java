4b664ec3728d0d6b70e30eb7eb6a90e3beb6a43b
Srdjan294 <srdjanfilipovic991@gmail.com>
2021-01-13 12:21:31 +0100

    riješen 1. zadatak

--- eclipse/Edunova04/src/edunova/zadaci/Zadatak1.java
+++ eclipse/Edunova04/src/edunova/zadaci/Zadatak1.java
@@ -7,4 +7,23 @@ public class Zadatak1 {
 	// Program unosi brojeve sve dok ne doðe broj -1
 	//Program ispisuje sumu svih unesenih bojeva
 	
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Unesi brojeve:");
+		
+		int i = 0, zbroj = 0;
+		
+		while(i != -1) {
+			i = input.nextInt();
+			if(i != -1) {
+				zbroj += i;
+			}
+		}
+		
+		System.out.println(zbroj);
+		
+		input.close();
+	}
 }
