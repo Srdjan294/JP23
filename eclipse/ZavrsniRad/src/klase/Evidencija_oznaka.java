7c7c364e14a0523115fd5385fa1656cf0d9d8b14
Srdjan294 <srdjanfilipovic991@gmail.com>
2021-01-25 16:33:21 +0100

    Vježbanje sa klasama i skromni početak završnog rada

---
+++ eclipse/ZavrsniRad/src/klase/Evidencija_oznaka.java
@@ -0,0 +1,33 @@
+package klase;
+
+public class Evidencija_oznaka {
+	
+	private int evidencija;
+	private int oznaka;
+	
+	//getteri i setteri
+	public int getEvidencija() {
+		return evidencija;
+	}
+	public void setEvidencija(int evidencija) {
+		this.evidencija = evidencija;
+	}
+	public int getOznaka() {
+		return oznaka;
+	}
+	public void setOznaka(int oznaka) {
+		this.oznaka = oznaka;
+	}
+	
+	//konstruktor
+	public Evidencija_oznaka(int evidencija, int oznaka) {
+		super();
+		this.evidencija = evidencija;
+		this.oznaka = oznaka;
+	}
+	
+	
+	
+	
+
+}
---
+++ eclipse/ZavrsniRad/src/module-info.java
@@ -0,0 +1,2 @@
+module zavrsniRad {
+}
