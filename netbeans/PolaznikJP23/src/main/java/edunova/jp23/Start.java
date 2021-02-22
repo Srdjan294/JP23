1ab1984766234bf71b106ce8108e9ec3ba4a152b
Srdjan294 <srdjanfilipovic991@gmail.com>
2021-02-22 15:16:19 +0100

    predavanje 17.2. postavljanje hibernate-a

---
+++ netbeans/PolaznikJP23/src/main/java/edunova/jp23/Start.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edunova.jp23;
+
+import edunova.jp23.util.HibernateUtil;
+import org.hibernate.Session;
+
+/**
+ *
+ * @author srdja
+ */
+public class Start {
+    
+    public static void main(String[] args) {
+        Session s = HibernateUtil.getSession();
+        
+        System.out.println(s.getMetamodel().getEntities().size());
+        
+    }
+    
+}
