cc3c45c01af53d711c9754430313127bba8d228d
Srdjan294 <srdjanfilipovic991@gmail.com>
2021-03-02 19:37:34 +0100

    predavanje 1.3. Obrada, ObradaSmjer sa kontrolom naziva i cijene, EdunovaException

--- netbeans/PolaznikJP23/src/main/java/edunova/jp23/Start.java
+++ netbeans/PolaznikJP23/src/main/java/edunova/jp23/Start.java
@@ -5,7 +5,13 @@
  */
 package edunova.jp23;
 
+import edunova.jp23.controller.ObradaSmjer;
+import edunova.jp23.model.Smjer;
+import edunova.jp23.util.EdunovaException;
 import edunova.jp23.util.InitialFixtures;
+import java.math.BigDecimal;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 
 
@@ -16,12 +22,24 @@ import edunova.jp23.util.InitialFixtures;
 public class Start {
     
     public static void main(String[] args) {
-        InitialFixtures.start();
         
+        //InitialFixtures.start();
         
+        Smjer s = new Smjer();
+        s.setNaziv("Dobar naziv");
+        s.setCijena(BigDecimal.ZERO);
+        ObradaSmjer os = new ObradaSmjer();
+        os.setEntitet(s);
         
+        try {
+            os.create();
+        } catch (EdunovaException e) {
+            System.out.println(e.getPoruka());
+        }
        
-        
+//        for(Smjer s : os.getPodaci()){
+//            System.out.println(s.getNaziv());
+//        }
         
         //System.out.println(s.getMetamodel().getEntities().size());
         
---
+++ netbeans/PolaznikJP23/src/main/java/edunova/jp23/controller/ObradaSmjer.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edunova.jp23.controller;
+
+import edunova.jp23.model.Smjer;
+import edunova.jp23.util.EdunovaException;
+import java.math.BigDecimal;
+import java.util.List;
+
+/**
+ *
+ * @author srdja
+ */
+public class ObradaSmjer extends Obrada<Smjer> {
+    
+    public ObradaSmjer(){
+        super();
+    }
+    
+    public ObradaSmjer(Smjer s){
+        super(s);
+    }
+
+    @Override
+    public List<Smjer> getPodaci() {
+        return session.createQuery("from Smjer").list();
+    }
+
+    @Override
+    protected void kontrolaCreate() throws EdunovaException {
+           kontrolaNaziv();
+           kontrolaCijena();
+    }
+
+    @Override
+    protected void kontrolaUpdate() {
+        
+    }
+
+    @Override
+    protected void kontrolaDelete() {
+        
+    }
+    
+     private void kontrolaNaziv() throws EdunovaException{
+        kontrolaNazivPostavljen();
+        kontrolaNazivDuzina(); 
+    }
+    
+    private void kontrolaNazivPostavljen() throws EdunovaException{
+        if(entitet.getNaziv() == null || entitet.getNaziv().isEmpty()){
+            throw new EdunovaException("Naziv mora biti postavljen");
+        }
+    }
+    
+    private void kontrolaNazivDuzina() throws EdunovaException{
+        if(entitet.getNaziv().length()>50){
+            throw new EdunovaException("Naziv predugačak");
+        }
+    }
+    
+    private void kontrolaCijena() throws EdunovaException{
+        if(entitet.getCijena() == null || entitet.getCijena().compareTo(BigDecimal.ZERO) <= 0){
+            throw new EdunovaException("Cijena mora biti postavljena i veća od nule");
+        }
+    }
+    
+    
+}
