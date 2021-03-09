ef57cf0d1de28e0ec2beebba5c81b1aad37bfc19
Srdjan294 <srdjanfilipovic991@gmail.com>
2021-03-09 16:07:17 +0100

    predavanje 8.3. početak view-a, autorizacija i operater

--- netbeans/PolaznikJP23/src/main/java/edunova/jp23/Start.java
+++ netbeans/PolaznikJP23/src/main/java/edunova/jp23/Start.java
@@ -5,15 +5,9 @@
  */
 package edunova.jp23;
 
-import edunova.jp23.controller.ObradaPolaznik;
-import edunova.jp23.controller.ObradaSmjer;
-import edunova.jp23.model.Polaznik;
-import edunova.jp23.model.Smjer;
-import edunova.jp23.util.EdunovaException;
-import edunova.jp23.util.InitialFixtures;
-import java.math.BigDecimal;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import edunova.jp23.view.Autorizacija;
+
+
 
 
 
@@ -25,40 +19,7 @@ public class Start {
     
     public static void main(String[] args) {
         
-        //InitialFixtures.start();
-        
-        /*Smjer s = new Smjer();
-        s.setNaziv("");
-        s.setCijena(BigDecimal.TEN);
-        ObradaSmjer os = new ObradaSmjer();
-        os.setEntitet(s);
-        
-        try {
-            os.create();
-        } catch (EdunovaException e) {
-            System.out.println(e.getPoruka());
-        }*/
-       
-//        for(Smjer s : os.getPodaci()){
-//            System.out.println(s.getNaziv());
-//        }
-        
-        Polaznik p = new Polaznik();
-        p.setIme("Pero");
-        p.setPrezime("Zaljubljeni");
-        p.setEmail("pero.zaljubljeni@gmail.com");
-        p.setOib("03201269716");
-        
-        ObradaPolaznik op = new ObradaPolaznik();
-        op.setEntitet(p);
-        
-        try {
-    
-              op.create();
-        } catch (EdunovaException ex) {
-            System.out.println(ex.getPoruka());
-        }
-              
+       new Autorizacija().setVisible(true);
     }
     
 }
---
+++ netbeans/PolaznikJP23/src/main/java/edunova/jp23/util/KreirajOperatera.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edunova.jp23.util;
+
+import edunova.jp23.controller.ObradaOperater;
+import edunova.jp23.model.Operater;
+import org.mindrot.jbcrypt.BCrypt;
+
+/**
+ *
+ * @author srdja
+ */
+public class KreirajOperatera {
+ 
+    public static void main(String[] args) {
+        
+        Operater o = new Operater();
+        o.setIme("Srđan");
+        o.setPrezime("Filipović");
+        o.setEmail("srdjanfilipovic991@gmail.com");
+        o.setLozinka(BCrypt.hashpw("edunova", BCrypt.gensalt()));
+        o.setOib("75919144249");
+        
+        ObradaOperater oo = new ObradaOperater();
+        oo.setEntitet(o);
+        
+        try {
+            oo.create();
+        } catch (EdunovaException e) {
+            System.out.println(e.getMessage());
+        }
+        
+    }
+}
