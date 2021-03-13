977efcdcd7edd30bbe2a11ff6d616f145841fc1b
Srdjan294 <srdjanfilipovic991@gmail.com>
2021-03-13 12:56:14 +0100

    kreiran SplashScreen

--- netbeans/ZavrsniRad/src/main/java/edunova/zavrsnirad/Start.java
+++ netbeans/ZavrsniRad/src/main/java/edunova/zavrsnirad/Start.java
@@ -12,6 +12,7 @@ import edunova.zavrsnirad.model.Vlasnik;
 import edunova.zavrsnirad.util.EdunovaException;
 import edunova.zavrsnirad.util.HibernateUtil;
 import edunova.zavrsnirad.view.Autorizacija;
+import edunova.zavrsnirad.view.SplashScreen;
 import org.hibernate.Session;
 
 /**
@@ -22,47 +23,7 @@ public class Start {
     
     public static void main(String[] args) {
         
-        //new Autorizacija().setVisible(true);
-        
-//        Vlasnik v = new Vlasnik();
-//        v.setIme("Srđan");
-//        v.setPrezime("Filipović");
-//        v.setSpol("muško");
-//        v.setOib("75919144249");
-//        
-//        ObradaVlasnik ov = new ObradaVlasnik();
-//        ov.setEntitet(v);
-//        
-//        try {
-//    
-//              ov.create();
-//        } catch (EdunovaException ex) {
-//            System.out.println(ex.getPoruka());
-//        }
-
-//          Auto a = new Auto();
-//          a.setGodiste(2000);
-//          a.setModel("Peugeot 307");
-//          a.setRegistracija("OS-555-LM");
-//          a.setVolumenRezervoara(45);
-//          
-//          
-//          ObradaAuto oa = new ObradaAuto();
-//          oa.setEntitet(a);
-//          
-//          try {
-//            oa.create();
-//        } catch (EdunovaException ex) {
-//              System.out.println(ex.getPoruka());
-//        }
-//        
-          
-          
-          
-        Session s = HibernateUtil.getSession();
-        System.out.println(s.getMetamodel().getEntities().size());
-        s.beginTransaction();
-        s.getTransaction().commit();
+        new SplashScreen().setVisible(true);
     }
     
 }
